6ae6f6d0baaa9d8a8f4cde98340f1ab8a7417975
Felix Gutierrez <fgutierrez2191@icloud.com>
2020-12-15 09:31:26 -0500

    fixed port

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 
+//require routes
+const { notes } = require('./develop/db/db.json');
+
+    // access to starter files from public
+app.use(express.static('public'));
+
+//     // parse incoming string or array data
+ app.use(express.urlencoded({ extended: true }));
+//     // parse incoming JSON data
+ app.use(express.json());
+ 
+//add route 
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+  });
 
 //listen method
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
