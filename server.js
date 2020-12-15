37700e2f39f9acceed241f55f5cea15b98d33559
Felix Gutierrez <fgutierrez2191@icloud.com>
2020-12-15 13:05:40 -0500

    added path

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
+//require routes
+const { notes } = require('./develop/db/db.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 
-//require routes
-const { notes } = require('./develop/db/db.json');
 
-
-
-    // access to starter files from public
+  // parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+   // parse incoming JSON data
+app.use(express.json());
+// access to starter files from public
 app.use(express.static('public'));
+
+
 
 //add route 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
     console.log("getrouteworks");
   });
+
 //post route
 app.post('/api/notes', (req, res) => {
     // req.body is where our incoming content will be
@@ -23,10 +30,6 @@ app.post('/api/notes', (req, res) => {
     res.json(req.body);
   });
 
-//     // parse incoming string or array data
- app.use(express.urlencoded({ extended: true }));
-//     // parse incoming JSON data
- app.use(express.json());
  
 
 //listen method
