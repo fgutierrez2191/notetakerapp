3ffb0c3368815ea013c0f751c62c9e93ec032043
Felix Gutierrez <fgutierrez2191@icloud.com>
2020-12-16 16:58:12 -0500

    routes work

--- server.js
+++ server.js
@@ -2,24 +2,35 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 //require routes
-const { notes } = require('./develop/db/db.json');
-const PORT = process.env.PORT || 3001;
+let jsonNotes = require('./db/db.json');
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-
+   // parse incoming JSON data
+   app.use(express.json());
 
   // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-   // parse incoming JSON data
-app.use(express.json());
+
 // access to starter files from public
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get('/notes', (req, res) => {
+ 
+
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
 
 
 //add route 
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
+    res.json(jsonNotes);
     console.log("getrouteworks");
   });
 
