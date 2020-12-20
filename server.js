a89d2376f687dd33cd25ed74c94400affb40abd6
Felix Gutierrez <fgutierrez2191@icloud.com>
2020-12-20 08:34:53 -0500

    fixed server.js

--- server.js
+++ server.js
@@ -1,49 +1,21 @@
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
-//require routes
-let jsonNotes = require('./db/db.json');
+const apiRoute = require('./routes/apiRoutes/index');
+const htmlRoute = require('./routes/htmlRoutes/index');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-   // parse incoming JSON data
-   app.use(express.json());
-
-  // parse incoming string or array data
+    // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
+    // parse incoming JSON data
+app.use(express.json());
+  
+app.use("/api", apiRoute);    
+app.use('/', htmlRoute);
 
-// access to starter files from public
+//use public files
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-app.get('/notes', (req, res) => {
- 
-
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-
-
-
-//add route 
-app.get('/api/notes', (req, res) => {
-    res.json(jsonNotes);
-    console.log("getrouteworks");
-  });
-
-//post route
-app.post('/api/notes', (req, res) => {
-    // req.body is where our incoming content will be
-    console.log(req.body);
-    res.json(req.body);
-  });
-
- 
-
-//listen method
+//listener
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-  });
+    console.log(`API server on port ${PORT}`)
+});
