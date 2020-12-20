199269bfdb5f52f9b0bb1a40d7c3796a9caf59e3
Felix Gutierrez <fgutierrez2191@icloud.com>
2020-12-20 08:26:42 -0500

    api routes modularized

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,24 @@
-// const router = require("express").Router();
+const router = require("express").Router();
+const { verifyNotes, createNotes } = require('../../lib/routeFunctions');
+const { notes } = require('./db/db.json');
+
+router.get("/notes", (req, res) => {
+    let results = notes;
+    res.json(results);
+});
 
 
+router.post("/notes", (req, res) => {
+
+    req.body.id = notes.length.toString();
+
+    if(!verifyNotes(req.body)) {
+        res.status(400).send('Your note is not corret formatted. Please fix it :)');
+    } else {
+        const note = createNotes(req.body, notes);
+        res.json(note);
+    }
+});
 
 
 
