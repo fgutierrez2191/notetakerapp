4f317cdda4d8e60a0d4ad1892cf13650c981835d
Felix Gutierrez <fgutierrez2191@icloud.com>
2020-12-26 05:52:30 -0500

    f

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const app = express();
 const apiRoute = require('./routes/apiRoutes/index');
 const htmlRoute = require('./routes/htmlRoutes/index');
+const app = express();
 const PORT = process.env.PORT || 3001;
 
     // parse incoming string or array data
